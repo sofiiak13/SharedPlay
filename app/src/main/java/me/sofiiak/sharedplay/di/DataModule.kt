1f8ce0d7acbe9d056bac1b07da3e30c1bc094b14
Sofiia Khutorna <sofiiakhutorna03@gmail.com>
2025-10-14 21:46:49 -0700

    add architecture structure:  - data layer: Repository, DataSource  - viewmodel layer: HomeViewModel.kt  - ui: HomeScreen.kt
     - cleaned old implementation
     - removed Firebase library dependency

---
+++ app/src/main/java/me/sofiiak/sharedplay/di/DataModule.kt
@@ -0,0 +1,11 @@
+package me.sofiiak.sharedplay.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+abstract class ViewModelModule {
+
+}
---
+++ app/src/main/java/me/sofiiak/sharedplay/ui/theme/HomeScreen.kt
@@ -0,0 +1,4 @@
+package me.sofiiak.sharedplay.ui.theme
+
+class MainScreen {
+}
